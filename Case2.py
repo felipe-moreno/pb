f8702e7a8cf05a50311682a1725895d6849d9cc3
Felipe Moreno <felipemorenolopes@gmail.com>
2023-08-14 09:23:25 -0300

    Case2 atualizado com requests melhores

--- Case2.py
+++ Case2.py
@@ -46,8 +46,56 @@ def validar_json(json_retorno):
 
 
 response1 = requests.get("https://jsonplaceholder.typicode.com/users/1")
-response2 = requests.post("https://jsonplaceholder.typicode.com/users")
-response3 = requests.put("https://jsonplaceholder.typicode.com/users/1")
+response2 = requests.post("https://jsonplaceholder.typicode.com/users", json={
+    "id": 3,
+    "name": "Felipe Moreno",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+})
+print(response2.json())
+
+response3 = requests.put("https://jsonplaceholder.typicode.com/users/3", json={
+    "id": 3,
+    "name": "Felipe Moreno Lorente Lopes",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+})
+print(response3.json())
+
 response4 = requests.delete("https://jsonplaceholder.typicode.com/users/1")
 
 validar_json(response1.json())
@@ -56,4 +104,3 @@ if (response1.ok and response2.ok and response3.ok and response4.ok):
     print("Retorno 200, ok.")
 else:
     print("Retorno diferente de 200, verificar.")
-
