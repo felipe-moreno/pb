43802689a3220126cd7fb495eff803bd9eb50966
Felipe Moreno <felipemorenolopes@gmail.com>
2023-09-07 19:45:34 -0300

    Case 1 Atualizado Printando canvas para comparar após cada clique de botão

--- Case1.py
+++ Case1.py
@@ -1,7 +1,8 @@
-from ler_url import url
+from teste_selenium import url
 
 endereco = url("https://the-internet.herokuapp.com/challenging_dom")
 
 endereco.clicar_botoes()
+endereco.validar_canvas()
 endereco.clicar_edit_delete()
 endereco.sair()
--- teste_selenium.py
+++ teste_selenium.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from PIL import Image
+from io import BytesIO
+import cv2
+import numpy as np
 
 class url():
     
@@ -20,6 +24,7 @@ class url():
                 print('Botao {} ok.'.format(i))
             else:
                 print('Botao nao mudou.')
+
 
     def clicar_edit_delete(self):
         for i in range(1,11):
@@ -42,5 +47,58 @@ class url():
                             print('Delete {} nao esta funcionando.'.format(i-1))
             print('')
         
+
+    def capturar_canvas(self,num):
+        canvas = self.motor.find_element(By.ID, 'canvas')
+        localizacao_canvas = canvas.location
+        tamanho_canvas = canvas.size
+        print_total = self.motor.get_screenshot_as_png()
+
+        image = Image.open(BytesIO(print_total))
+
+        left = localizacao_canvas['x']
+        top = localizacao_canvas['y']
+        right = localizacao_canvas['x'] + tamanho_canvas['width']
+        bottom = localizacao_canvas['y'] + tamanho_canvas['height']
+
+        image = image.crop((left, top, right, bottom))
+        image.save('canvas{}.png'.format(num))
+
+
+    @staticmethod
+    def comparar_imagens():
+        image1 = cv2.imread('canvas1.png')
+        image2 = cv2.imread('canvas2.png')
+        image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+        image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+            
+        def mse(img1, img2):
+            h, w = img1.shape
+            diff = cv2.subtract(img1, img2)
+            err = np.sum(diff**2)
+            mse = err/(float(h*w))
+            return mse     
+        
+        error = mse(image1, image2)
+        return error
+
+
+    def validar_canvas(self):
+        for i in range(1,4):
+            self.capturar_canvas(1)
+
+            botao = self.motor.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/a[{}]'.format(i))
+            botao.click()
+
+            self.capturar_canvas(2)
+
+            diferenca = self.comparar_imagens()
+
+            if (diferenca > 0):
+                print('Botao {} alterou o Canvas, portanto está funcionando.'.format(i))
+            else:
+                print('Botao {} não alterou o Canvas, portanto não está funcionando.'.format(i))
+
+
     def sair(self):
         self.motor.quit()
